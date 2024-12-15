0f6a86cc348e3cadb5fafd91882d394a314ec49e
RiTa-23 <rita.tifrt@gmail.com>
2024-12-15 11:12:37 +0900

    usersテーブルにガチャ系カラムを追加（作り直し）

--- database/migrations/2024_12_15_014337_add_column_to_user.php
+++ database/migrations/2024_12_15_014337_add_column_to_user.php
@@ -4,28 +4,36 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('gacha_num')->default(2);
-            $table->date('last_gacha_date')->nullable();
+            // 新しいカラムを追加
+            $table->dateTime('last_pack_drawn_at')->default(DB::raw('CURRENT_TIMESTAMP'))->after('updated_at');
+            $table->unsignedInteger('available_pack_draws')->default(2)->after('last_pack_drawn_at');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
-            $table->dropColumn('gacha_num'); // 元に戻す処理
-            $table->dropColumn('last_gacha_date'); // 元に戻す処理
+            // 新しいカラムを削除
+            if (Schema::hasColumn('users', 'last_pack_drawn_at')) {
+                $table->dropColumn('last_pack_drawn_at');
+            }
+            if (Schema::hasColumn('users', 'available_pack_draws')) {
+                $table->dropColumn('available_pack_draws');
+            }
         });
     }
 };
